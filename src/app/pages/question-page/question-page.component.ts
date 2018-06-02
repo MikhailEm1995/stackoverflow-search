8181a8dff93b49401f795bfbf779aa770ec76de6
Емченко Михаил <memchenko@htc-cs.ru>
2018-06-02 16:01:28 +0400

    FEATURES: - css files changed to scss

--- src/app/pages/question-page/question-page.component.ts
+++ src/app/pages/question-page/question-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-question-page',
   templateUrl: './question-page.component.html',
-  styleUrls: ['./question-page.component.css']
+  styleUrls: ['./question-page.component.scss']
 })
 export class QuestionPageComponent implements OnInit {
 
