5ca3cecf5c00181adecab7123dc9637b63e813c1
memchenko <emchenko.mikhail@mail.ru>
2018-06-03 15:33:51 +0400

    FEATURE: - added sorting to table component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -13,6 +13,10 @@ export class TableComponent {
   @Input() onTopicClick: Function;
   @Input() onTagClick: Function;
 
+  private isAuthorDesc = false;
+  private isTitleDesc = false;
+  private isAnswersDesc = false;
+
   constructor() {}
 
   handleAuthorClick(userId: number): void {
@@ -25,5 +29,55 @@ export class TableComponent {
 
   handleTagClick(tag: string): void {
     this.onTagClick(tag);
+  }
+
+  sortByAuthor(): void {
+    this.isAuthorDesc = !this.isAuthorDesc;
+
+    this.questions = this.questions.sort((curr, next) => {
+      const currAuthor = curr.display_name.toLowerCase().charCodeAt(0);
+      const nextAuthor = next.display_name.toLowerCase().charCodeAt(0);
+
+      switch (true) {
+        case isNaN(currAuthor) || isNaN(nextAuthor): return 0;
+        case currAuthor === nextAuthor: return 0;
+        case ((this.isAuthorDesc && currAuthor < nextAuthor) ||
+          (!this.isAuthorDesc && currAuthor > nextAuthor)): return -1;
+        default: return 1;
+      }
+    });
+  }
+
+  sortByTitle(): void {
+    this.isTitleDesc = !this.isTitleDesc;
+
+    this.questions = this.questions.sort((curr, next) => {
+      const currTitle = curr.title.toLowerCase().charCodeAt(0);
+      const nextTitle = next.title.toLowerCase().charCodeAt(0);
+
+      switch (true) {
+        case isNaN(currTitle) || isNaN(nextTitle): return 0;
+        case currTitle === nextTitle: return 0;
+        case ((this.isTitleDesc && currTitle < nextTitle) ||
+          (!this.isTitleDesc && currTitle > nextTitle)): return -1;
+        default: return 1;
+      }
+    });
+  }
+
+  sortByAnswers(): void {
+    this.isAnswersDesc = !this.isAnswersDesc;
+
+    this.questions = this.questions.sort((curr, next) => {
+      const currAnswers = curr.answer_count;
+      const nextAnswers = next.answer_count;
+
+      switch (true) {
+        case currAnswers === nextAnswers: return 0;
+        case ((this.isAnswersDesc && currAnswers < nextAnswers) ||
+          (!this.isAnswersDesc && currAnswers > nextAnswers)): return -1;
+        default: return 1;
+      }
+    });
   }
 }
